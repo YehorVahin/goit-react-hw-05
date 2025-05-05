49c59b9f46a2769fc3e2e1a44ee6d626a273f188
Yehor Vahin <davyjohns322@gmail.com>
2025-05-05 14:27:58 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
 import FeedBackForm from './components/FeedBackForm/FeedBackForm'
+import { useState } from 'react'
+import ArticleList from './components/ArticleList/ArticleList'
+import { fetchArticlesWithTopics } from './articles-api'
+import { SearchForm } from './components/SearchForm/SearchForm'
 
 
 function App() {
-  
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopics(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
     <>
+      <SearchForm onSearch={handleSearch}/>
       <FeedBackForm></FeedBackForm>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </>
   )
 }
--- src/components/FeedBackForm/FeedBackForm.jsx
+++ src/components/FeedBackForm/FeedBackForm.jsx
@@ -1,11 +1,50 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
 
+const initialValues = {
+    username: "",
+    email: ""
+}
+const FeedbackSchema = Yup.object().shape({
+    username: Yup.string().min(2, "Too short!").max(50, "Too long!").required("Required!"),
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
+	level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required")
+});
 const FeedBackForm = () => {
+    const handleSubmit = (values, actions) => {
+        console.log(values);
+        actions.resetForm();
+    };
+    const nameFieldId = useId();
+    const emailFieldId = useId();
+    const msgFieldId = useId();
+    const levelFieldId = useId();
     return(
-        <Formik initialValues={{}} onSubmit={() => {}}>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
             <Form>
-                <Field type="text" name="username"/>
-                <Field type="email" name="email"/>
+                <div>
+                <label htmlFor={nameFieldId}>Username</label>
+                <Field type="text" name="username" id={nameFieldId}/>
+                <ErrorMessage name="username" component="span"/>
+                </div>
+                <div>
+                <label htmlFor={emailFieldId}>E-mail</label>
+                <Field type="email" name="email" id={emailFieldId}/>
+                <ErrorMessage name="email" component="span"/>
+                </div>
+                <div>
+                <label htmlFor={msgFieldId}>Message</label>
+                <Field as="textarea" name="message" id={msgFieldId} rows="5"/>
+                <ErrorMessage name="message" component="span"/>
+                </div>
+                <label htmlFor={levelFieldId}>Service satisfaction level</label>
+                <Field as="select" name="level" id={levelFieldId}>
+                    <option value="good">Good</option>
+                    <option value="neutral">Neutral</option>
+                    <option value="bad">Bad</option>
+                </Field>
                 <button type="submit">Submit</button>
             </Form>
         </Formik>
